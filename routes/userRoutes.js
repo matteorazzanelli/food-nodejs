8d05405f899e429387523447e7a4cd890afb2665
matteo <matteo.razzanelli89@gmail.com>
2023-09-21 15:12:50 +0200

    minor

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,24 +2,24 @@ const express = require('express');
 const router = express.Router();
 
 // load product controller
-const userController = require('../controllers/userController');
+const {userController} = require('../controllers/userController');
 
 //middleware to handle req.body
 router.use(express.json());
 
 router.get('/', (req, res)=>{
   console.log('get all users');
-  userController.listUsers();
+  return userController.listUsers(req, res);
 })
 
 router.get('/:id',(req, res)=>{
   console.log('get product : '+req.params.id);
-  userController.findUser(req.params.id);
+  return userController.findUser(req.params.id);
 })
 
 router.post('/', (req,res)=>{
   console.log('create user : '+req.body);
-  userController.addUser(req.body);
+  return userController.addUser(req.body);
 })
 
 module.exports = router;
