3a26afa2999a167ae5cccecf55f445d84fd5a2cc
matteo <matteo.razzanelli89@gmail.com>
2023-09-24 17:01:21 +0200

    feat: adding order routes

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,13 @@ const router = express.Router();
 
 const productRoutes = require('./productRoutes');
 const userRoutes = require('./userRoutes');
+const orderRoutes = require('./orderRoutes')
 
 // using specific routes middleware for products and users
 router
   .get('/', ()=>{console.log('-------->HOME<----------')})
   .use('/products', productRoutes)
-  .use('/users', userRoutes);
+  .use('/users', userRoutes)
+  .use('/orders', orderRoutes);
 
 module.exports = router;
---
+++ routes/orderRoutes.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+
+// load order controller
+const { orderController } = require('../controllers/orderController');
+
+//middleware to handle req.body
+router.use(express.json());
+
+router.get('/', (req, res) => {
+  return orderController.listOrders(res);
+})
+
+router.post('/', (req, res) => {
+  const {content} = req.body;
+  return orderController.insertOrder(content, res);
+})
+
+router.put('/:id', (req,res) => {
+  const {id} = req.params;
+  const {content} = req.body;
+  // per ogni campo del req.body modificare il campo del record
+  return orderController.modifyOrder(id, content, res);
+})
+
+router.delete('/:id', (req,res) => {
+  const {id} = req.params;
+  return orderController.deleteOrder(id, res);
+})
+
+module.exports = router;
